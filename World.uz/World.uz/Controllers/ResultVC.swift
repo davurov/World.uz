01c9654d1a72ef0742cacb9183c3c058ccbf6982
davurov <113560218+davurov@users.noreply.github.com>
2023-04-01 18:29:04 +0500

    ResultVC started

---
+++ World.uz/World.uz/Controllers/ResultVC.swift
@@ -0,0 +1,17 @@
+//
+//  ResultVC.swift
+//  World.uz
+//
+//  Created by Abduraxmon on 01/04/23.
+//
+
+import UIKit
+
+class ResultVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
